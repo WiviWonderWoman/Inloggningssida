65239f08627d793db136b2c1952d6074591b5443
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-08 12:41:15 +0200

    Kommentarer som pseudokod

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 console.log('test');
 const app = {
-    key:"test",
-    value: "1234",
+    // key:"test",
+    // value: "1234",
     namnFält: document.createElement('input'),
     namn: namnFält.value,
     lösenFält: document.createElement('input'),
@@ -10,7 +10,37 @@ const app = {
     meny: document.getElementById('meny'),
     inKnapp: document.createElement('button'),
     utKnapp: document.createElement('button'),
-    inText: document.createTextNode("Vad trevligt att du hittade hit! Logga gärna in!"),
+    inText: document.createTextNode("Vad trevligt att du loggade hit!"),
     utTex: document.createTextNode("Du är nu utloggad. Välkommen åter!"),
     felText: document.createTextNode("Hoppsan, har du glömt koden?"),
 }
+//OM localStorage är tomt
+    //VISA namnFält
+    //VISA lösenFält
+    //VISA inKnapp
+
+    //LYSSNA på inKnapp
+        //VID klick på inKnapp
+
+            //OM namn === test && lösen === 1234 
+                //SPARA i localStorage
+                //VISA inText
+                //VISA utKnapp
+
+                //LYSSNA på utKnapp
+                    //VID klick på utKnapp
+
+                        //TÖM localStorage
+                        //VISA utText
+            //ANNARS
+                //VISA felText
+
+//ANNARS
+    //VISA inText
+    //VISA utKnapp
+
+    //LYSSNA på utKnapp
+        //VID klick på utKnapp
+
+            //TÖM localStorage
+            //VISA utText
